ecd070bc694a6a7acc8111f91a7a1953b4c7852a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-07-14 23:52:26 -0700

    refactor: use resourceful route for StaticPages index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  resources :static_pages, only: %w[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
