a887bad70cf716bcf588299b23484b2822d86469
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-07-15 20:10:14 -0700

    feat: store response for user photos upon searching in StaticPagesController#index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
+    return unless params[:user_id]
+
+    flickr = Flickr.new
+    @user_photos = flickr.photos.search user_id: params[:user_id]
   end
 end
